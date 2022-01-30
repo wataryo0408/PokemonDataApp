67db24a33a04d74810d49cff1f5d60eaded27ae1
wataryo0408 <wataryo0408@icloud.com>
2022-01-30 21:58:18 +0900

    Initial Commit

---
+++ PokemonDataApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PokemonDataApp
+//
+//  Created by 渡邉凌 on 2022/01/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
