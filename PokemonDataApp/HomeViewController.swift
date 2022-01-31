adc45f7a47742f304bdc1c6388f6a2ce7557769d
wataryo0408 <wataryo0408@icloud.com>
2022-01-31 19:10:39 +0900

    tableViewの定義

--- PokemonDataApp/HomeViewController.swift
+++ PokemonDataApp/HomeViewController.swift
@@ -9,9 +9,52 @@ import Foundation
 import UIKit
 
 class HomeViewController: UIViewController{
+   
+    
+    var models = [
+    
+        "フシギダネ",
+        "フシギソウ",
+        "フシギバナ"
+
+    ]
+    
+    var pokemonTableView = UITableView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .red
+        
+        pokemonTableView = UITableView(frame: self.view.bounds, style: .plain)
+        pokemonTableView.backgroundColor = .blue
+        pokemonTableView.showsVerticalScrollIndicator = false
+        pokemonTableView.delegate = self
+        pokemonTableView.dataSource = self
+        
+        self.view.addSubview(pokemonTableView)
     }
+}
+
+
+extension HomeViewController: UITableViewDelegate, UITableViewDataSource{
+    
+    
+    //cellの数
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return models.count
+    }
+    
+    //cellの高さ
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 100
+    }
+    //cellの定義
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .default, reuseIdentifier: nil)
+        cell.textLabel?.text = models[indexPath.row]
+        
+        return cell
+    }
+    
+    
 }
